5b7f5d98831f40afef4dffac510a5434d2824fae
dannegm <im@danielgarcia.me>
2026-06-07 13:33:45 -0600

    feat: increase rate limit to 1500 requests per 5 minutes

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const app = express();
 
 const limiter = rateLimit({
     windowMs: 5 * 60 * 1000, // 5 minutes
-    limit: 1000, // Limit each IP to 1000 requests per `window` (here, per 5 minutes).
+    limit: 1500, // Limit each IP to 1500 requests per `window` (here, per 5 minutes).
     standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
     ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
