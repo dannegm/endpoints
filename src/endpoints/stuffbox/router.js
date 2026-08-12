00ca2e83ecb5bdc9821bca02d02996cb335df9d8
dannegm <im@danielgarcia.me>
2026-08-11 23:04:26 -0600

    feat(stuffbox): enhance photo retrieval endpoint to support image resizing with aspect ratio

--- src/endpoints/stuffbox/router.js
+++ src/endpoints/stuffbox/router.js
@@ -4,16 +4,18 @@ import sharp from 'sharp';
 const router = Router();
 
 const SIZE_WHITELIST = [48, 200, 360, 600];
+const RATIO_WHITELIST = [1, 2, 3];
 const R2_PUBLIC_URL = process.env.STUFFBOX__R2_PUBLIC_URL;
 
-router.get('/photo/:sizeId/*', async (req, res) => {
-    const { sizeId } = req.params;
+router.get('/photo/:sizeId/:ratio/*', async (req, res) => {
+    const { sizeId, ratio } = req.params;
     const key = req.params[0];
 
     const isOriginal = sizeId === 'o';
-    const size = SIZE_WHITELIST[Number(sizeId) - 1];
+    const baseSize = SIZE_WHITELIST[Number(sizeId) - 1];
+    const ratioMultiplier = RATIO_WHITELIST.includes(Number(ratio)) ? Number(ratio) : null;
 
-    if (!isOriginal && !size) {
+    if (!ratioMultiplier || (!isOriginal && !baseSize)) {
         return res.status(404).end();
     }
 
@@ -31,6 +33,8 @@ router.get('/photo/:sizeId/*', async (req, res) => {
     if (isOriginal) {
         return res.send(buffer);
     }
+
+    const size = baseSize * ratioMultiplier;
 
     const output = await sharp(buffer)
         .resize(size, size, { fit: 'outside', withoutEnlargement: true })
